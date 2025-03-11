17b99985d1a1eaf4b4eaa0b446baba3106d0abf6
ANANTTIWARI01 <bhaukalbaba45@gmail.com>
2025-03-11 11:33:02 +0530

    add dist

--- Frontend/axiosconfig.js
+++ Frontend/axiosconfig.js
@@ -1,4 +1,4 @@
 import axios from "axios";
 
-const instance = axios.create({baseURL:`http://localhost:8080/api`})
+const instance = axios.create({baseURL:`https://api-fullstack-project-back.onrender.com/api`})
 export default instance;
