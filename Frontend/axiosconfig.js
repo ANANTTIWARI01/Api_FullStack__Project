f59974a6f4587cd920d877cf147f460f7a83b6cf
ANANTTIWARI01 <bhaukalbaba45@gmail.com>
2025-03-19 16:11:49 +0530

    testing

--- Frontend/axiosconfig.js
+++ Frontend/axiosconfig.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-// const instance = axios.create({baseURL:`https://api-fullstack-project-back.onrender.com/api`})
-const instance = axios.create({baseURL:`http://localhost:8080/api`})
+const instance = axios.create({baseURL:`https://api-fullstack-project-back.onrender.com/api`})
+// const instance = axios.create({baseURL:`http://localhost:8080/api`})
 export default instance;
